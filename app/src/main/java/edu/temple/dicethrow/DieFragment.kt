434392abd8cd620e9e6e0a08390566d8b9399ca2
BennyQian1 <tuq28313@temple.edu>
2025-03-19 16:10:50 -0400

    -Fixed the bug of improper dice rolls -Added function to button -Keeps die roll when screen flips -Added Multidice and Multidice

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -1,29 +1,36 @@
 package edu.temple.dicethrow
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import kotlin.random.Random
 
 class DieFragment : Fragment() {
 
-    val DIESIDE = "sidenumber"
-
-    val PrevRoll = "PREVROLL"
-
-    var RollValue = 0
-
-    var anInt = 0;
-
+    private val PrevRoll = "PREVROLL"
+    private var RollValue = 0
     lateinit var dieTextView: TextView
+    private var dieSides: Int = 6
 
-    var dieSides: Int = 6
+    companion object {
+        private val DIESIDE = "sidenumber"
+        fun newInstance(numSides: Int): DieFragment {
+            val fragment = DieFragment()
+            val args = Bundle()
+            args.putInt("numSides", numSides)
+            fragment.arguments = args
+            return fragment
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        promptForDieSides()
         arguments?.let {
             it.getInt(DIESIDE).run {
                 dieSides = this
@@ -61,8 +68,22 @@ class DieFragment : Fragment() {
     }
 
     fun throwDie() {
-        RollValue = Random.nextInt(dieSides + 1)
-
+        RollValue = Random.nextInt(1, dieSides + 1)
         dieTextView.text = RollValue.toString()
     }
+    private fun promptForDieSides() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Enter Number of Sides for the Die")
+        // Set up the input
+        val input = EditText(requireContext())
+        builder.setView(input)
+        // Set up the buttons
+        builder.setPositiveButton("OK") { dialog, which ->
+            val inputValue = input.text.toString()
+            dieSides = inputValue.toIntOrNull() ?: 6 // Default to 6 if input is invalid
+        }
+        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+        builder.show()
+    }
 }
+
